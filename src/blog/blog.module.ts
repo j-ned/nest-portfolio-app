0c6c59b5f213c59c41e3e088483be4db52094df3
J - Ned <contact@nedellec-julien.fr>
2026-09-01 19:41:54 +0200

    feat(blog): ajoute AppConfigModule à BlogModule
    - Permet à BlogModule d'accéder aux configurations globales via AppConfigModule.
    - Maintient une meilleure extensibilité et modularité du code.

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
+import { AppConfigModule } from '../config/app-config.module';
 import { AuthModule } from '../auth/auth.module';
 import { multerConfig } from '../common/multer.config';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 
 @Module({
-  imports: [AuthModule, MulterModule.register(multerConfig(5))],
+  imports: [
+    AuthModule,
+    AppConfigModule,
+    MulterModule.register(multerConfig(5)),
+  ],
   controllers: [BlogController],
   providers: [BlogService],
 })
